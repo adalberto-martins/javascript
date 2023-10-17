d27f97f00e64b63ef7a5f2054f44aa5cee5cf9de
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2023-10-16 22:38:56 -0300

    criação ex08,09,10

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 12
+var vel = 78.2
 console.log(`A velocidade do seu carro é ${vel}km/h`)
-if (vel > 60) {
+if (vel > 60) { // condição simples!
     console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
 }
 console.log('Dirija sempre usando cinto de segurança!')
