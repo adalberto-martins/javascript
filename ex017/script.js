ec0c96ff633d90b94194f1ed1dee7c3cb003736e
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2023-10-28 01:10:04 -0300

    update

--- ex017/script.js
+++ ex017/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function tabuada() {
+    let nun = document.getElementById('txtn')
+    let tab = document.getElementById('Seltab')
+    if (nun.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(nun.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+
+    }
+
+}
